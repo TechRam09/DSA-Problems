a36e362ac9f5927169308670d16f3cfbe5ad5335
Shreeram N S <144241655+TechRam09@users.noreply.github.com>
2025-07-09 02:31:56 +0530

    Time: 5 ms (56.5%), Space: 42.7 MB (97.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,34 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 if(s.length() != t.length()) return false;
-        HashMap<Character,Integer> smap = new HashMap<>();
-        HashMap<Character,Integer> tmap = new HashMap<>();
+        // HashMap<Character,Integer> smap = new HashMap<>();
+        // HashMap<Character,Integer> tmap = new HashMap<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     char schar = s.charAt(i);
+        //     char tchar = t.charAt(i);
+        //     smap.put(schar,smap.getOrDefault(schar,0)+1);
+        //     tmap.put(tchar,tmap.getOrDefault(tchar,0)+1);
+        // }
+
+        // for(char c : smap.keySet()){
+        //     if (!smap.get(c).equals(tmap.get(c))){
+        //         return false;
+        //     }
+
+        // }
+        // return true;
+
+         if(s.length() != t.length()) return false;
+
+        int[] arr = new int[26];
+
         for (int i = 0; i < s.length(); i++) {
-            char schar = s.charAt(i);
-            char tchar = t.charAt(i);
-            smap.put(schar,smap.getOrDefault(schar,0)+1);
-            tmap.put(tchar,tmap.getOrDefault(tchar,0)+1);
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for(char c : smap.keySet()){
-            if (!smap.get(c).equals(tmap.get(c))){
-                return false;
-            }
-
+        for (int num:arr){
+            if(num !=0) return false;
         }
         return true;
     }
