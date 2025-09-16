1eb7c77e1594493880abc586571ca2a85145004e
Shreeram N S <144241655+TechRam09@users.noreply.github.com>
2025-09-16 12:58:04 +0530

    Time: 0 ms (100%), Space: 45.2 MB (85.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,34 +1,12 @@
 class Solution {
     public static int missingNumber(int[] arr) {
-    //cycle sort algo
         int n = arr.length;
-
-        int i =0 ;
-        while(i<n){
-            //correct index is equal current index not -1, since range start from 0
-            int correct = arr[i];
-
-            // the comparison should be value less than N not equal to N, if equal skips
-            //because index N does not exist
-            if(arr[i] < n && arr[i] != arr[correct]){
-                swap(arr,i,correct);
-            }else {
-                i++;
-            }
+        int sum1 = (n*(n+1))/2;
+        int sum2 =0;
+        for(int num:arr){
+            sum2+=num;
         }
 
-        for (int j = 0; j < n; j++) {
-            if(arr[j] != j){
-                return j;
-            }
-        }
-
-        return n;
-    }
-
-    private static void swap(int[] arr, int i, int correct) {
-        int temp = arr[i];
-        arr[i] = arr[correct];
-        arr[correct] = temp;
+        return sum1-sum2;
     }
 }
