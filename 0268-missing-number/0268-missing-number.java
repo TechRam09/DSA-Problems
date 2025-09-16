204b81369b38a0b522cef226969058e116c78105
Shreeram N S <144241655+TechRam09@users.noreply.github.com>
2025-09-16 11:27:06 +0530

    Time: 1 ms (33.14%), Space: 45.2 MB (85.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,34 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int xor1 = 0; 
-        int xor2 = 0;
-        for(int i =0;i<=n;i++){
-            xor1 ^= i;
+    public static int missingNumber(int[] arr) {
+    //cycle sort algo
+        int n = arr.length;
+
+        int i =0 ;
+        while(i<n){
+            //correct index is equal current index not -1, since range start from 0
+            int correct = arr[i];
+
+            // the comparison should be value less than N not equal to N, if equal skips
+            //because index N does not exist
+            if(arr[i] < n && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else {
+                i++;
+            }
         }
 
-        for(int num : nums){
-            xor2 ^= num;
+        for (int j = 0; j < n; j++) {
+            if(arr[j] != j){
+                return j;
+            }
         }
 
-        return (xor1 ^ xor2);
+        return n;
+    }
+
+    private static void swap(int[] arr, int i, int correct) {
+        int temp = arr[i];
+        arr[i] = arr[correct];
+        arr[correct] = temp;
     }
 }
