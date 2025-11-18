0ed60c0380ec620eb4e020b492e497ab6a94e0dc
Shreeram N S <144241655+TechRam09@users.noreply.github.com>
2025-11-18 17:49:03 +0530

    Time: 3 ms (66.21%), Space: 43.4 MB (11.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack();
+        for(char c:arr){
+            if(isOpeningBracket(c)){
+                stack.push(c);
+            }else{
+                if(stack.size() == 0){
+                    return false;
+                }
+                if(matching(stack.peek(),c)){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.size() == 0;
+    }
+
+    public boolean isOpeningBracket(char c){
+        switch(c){
+            case '(':
+            case '[':
+            case '{':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean matching(char c, char d){
+        if(c == '(' && d == ')'){
+            return true;
+        }else if(c == '[' && d == ']'){
+            return true;
+        }else if(c == '{' && d == '}'){
+            return true;
+        }else{
+           return false;
+        }
+    }
+}
